bb267e9c6b6e2f407b7a42163e06d5931ff30fe0
Melike <melikedemirci23@gmail.com>
2024-05-03 16:49:11 +0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {
+window.onscroll = function () {
     scrollFunction();
 };
 
@@ -10,3 +10,19 @@ function scrollFunction() {
         navbar.classList.remove("scrolled");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const konumElement = document.querySelector('.konum');
+    const konumAdres = encodeURIComponent("Yavuz Mahallesi, Peştemalcı Caddesi, Bayol Pasajı No:16/34 Süleymanpaşa/Tekirdağ");
+    konumElement.addEventListener('click', function () {
+        window.open(`https://www.google.com/search?q=${konumAdres}`, '_blank');
+    });
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const logo = document.getElementById('home');
+//     logo.addEventListener('click', function () {
+//         window.location.href = 'index.html'; // veya anasayfanın URL'sini girin
+//     });
+// });
