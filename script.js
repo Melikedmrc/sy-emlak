5a8767b34156e735b42c09de4a2dcca861602fe9
Melike <melikedemirci23@gmail.com>
2024-05-12 23:02:13 +0300

    responsive design
    Added media queries for responsive design

--- script.js
+++ script.js
@@ -11,14 +11,49 @@ function scrollFunction() {
     }
 }
 
+//---KONUMA TIKLAMA---//
+function openLocation() {
+    const konumAdres = encodeURIComponent("Yavuz Mahallesi, Peştemalcı Caddesi, Bayol Pasajı No:16/34 Süleymanpaşa/Tekirdağ");
+    window.open(`https://www.google.com/search?q=${konumAdres}`, '_blank');
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const konumElement = document.querySelector('.konum');
-    const konumAdres = encodeURIComponent("Yavuz Mahallesi, Peştemalcı Caddesi, Bayol Pasajı No:16/34 Süleymanpaşa/Tekirdağ");
     konumElement.addEventListener('click', function () {
-        window.open(`https://www.google.com/search?q=${konumAdres}`, '_blank');
+        openLocation();
     });
 });
 
+//---MOBİL BOYUTA GÖRE LOGO DEĞİŞİMİ---//
+
+// Ekran boyutunu dinleyen bir event listener ekleyelim
+window.addEventListener('resize', function() {
+    // Ekran genişliğini kontrol edelim
+    if (window.innerWidth <= 480) { // Mobil boyut
+        // İlk logo görüntüsünü gizleyelim
+        document.querySelector('.first_logo').style.display = 'none';
+        // Mobil logo görüntüsünü gösterelim
+        document.querySelector('.second_logo').style.display = 'block';
+    } else { // Diğer boyutlar
+        // İlk logo görüntüsünü gösterelim
+        document.querySelector('.first_logo').style.display = 'block';
+        // Mobil logo görüntüsünü gizleyelim
+        document.querySelector('.second_logo').style.display = 'none';
+    }
+});
+
+// Sayfa yüklendiğinde de kontrol edelim
+if (window.innerWidth <= 480) { // Mobil boyut
+    // İlk logo görüntüsünü gizleyelim
+    document.querySelector('.first_logo').style.display = 'none';
+    // Mobil logo görüntüsünü gösterelim
+    document.querySelector('.second_logo').style.display = 'block';
+} else { // Diğer boyutlar
+    // İlk logo görüntüsünü gösterelim
+    document.querySelector('.first_logo').style.display = 'block';
+    // Mobil logo görüntüsünü gizleyelim
+    document.querySelector('.second_logo').style.display = 'none';
+}
 
 // document.addEventListener('DOMContentLoaded', function () {
 //     const logo = document.getElementById('home');
